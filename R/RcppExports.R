417315adee0ac91f8de8ad6b8c41712c33718044
schi006 <sunyichi@umich.edu>
2019-11-26 01:17:40 -0500

    test

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,22 +2,22 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 cpp_linear_fit <- function(x, y) {
-    .Call('_penaltyRegression_cpp_linear_fit', PACKAGE = 'penaltyRegression', x, y)
+    .Call(`_penaltyRegression_cpp_linear_fit`, x, y)
 }
 
 cpp_ridge_fit <- function(x, y, lambda) {
-    .Call('_penaltyRegression_cpp_ridge_fit', PACKAGE = 'penaltyRegression', x, y, lambda)
+    .Call(`_penaltyRegression_cpp_ridge_fit`, x, y, lambda)
 }
 
 cpp_softmax <- function(x, y) {
-    .Call('_penaltyRegression_cpp_softmax', PACKAGE = 'penaltyRegression', x, y)
+    .Call(`_penaltyRegression_cpp_softmax`, x, y)
 }
 
 cpp_lasso_fit <- function(x, y, lambda, tol, max_iters) {
-    .Call('_penaltyRegression_cpp_lasso_fit', PACKAGE = 'penaltyRegression', x, y, lambda, tol, max_iters)
+    .Call(`_penaltyRegression_cpp_lasso_fit`, x, y, lambda, tol, max_iters)
 }
 
 cpp_predict <- function(x, beta) {
-    .Call('_penaltyRegression_cpp_predict', PACKAGE = 'penaltyRegression', x, beta)
+    .Call(`_penaltyRegression_cpp_predict`, x, beta)
 }
 
--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -1,3 +1,3 @@
 test_that("linear regression works", {
-  expect_equal(linear_fit(2,4)$beta, 2)
+  expect_equal(as.numeric(linear_fit(matrix(c(2,3)),matrix(c(4,6)))$beta), 2)
 })
